8fb74b6c43b88f99f9bbb9f81765a724749d6494
D Spaviero <danspavier@gmail.com>
2025-12-02 00:55:49 -0300

    inputs and variables

--- src/thermo_utils/cli.py
+++ src/thermo_utils/cli.py
@@ -1,11 +1,14 @@
 import shutil
 import sys
+from colorama import init
+init()
+
 
 LOGO = r"""
               (        )  (        )      (          )    
    )   (      )\    ( /(  )\    ( /(      )\ )     (/(   )
   (\   ))\   ( ((   )\())((_)  ))\(_\  ( ((_)(   )\ )\  ((
-   (-\_) )\ )\/_ )\((_)\ (_ )\)(_)\(( /  )\ _((_))((_)| \_)-)\
+   (-)_) )\ )\/_ )\((_)\ (_ )\)(_)\(()/  )\ _((_))((_)| \_)-)\
    / /_/ /(_|))_))((_)_((_))_((_) (_)\ \) ) ))((_))/ /(_)/ /\__)
   / __/ __ \/ _ \/ ___/ __ `__ \/ __ \_(\/__/ / / / __/ / / ___/
 / /_/ / / /  __/ /  / / / / / / /_/ /_____/ /_/ / /_/ / (__  )
@@ -13,7 +16,7 @@ LOGO = r"""
 """
 VERSION = "version 0.1.0"
 MESSAGE = "This program uses bar, cm³, mol and K.\nPlease type your input values using these units.\n\n"
-COMMANDS = "Type a number:\n\n1 - PVT calculus (ideal gases equation)\n2 - PVT calculus (virial equation)\n3 - PVT calculus (Peng-Robinson equation)\n4 - Fugacity\n5 - Activity coefficient\n6 - Margules parameters using experimental data (T constant)"
+COMMANDS = "Type a number:\n\n1 - PVT calculus (ideal gases equation)\n2 - PVT calculus (virial equation)\n3 - PVT calculus (Peng-Robinson equation)\n4 - Fugacity\n5 - Activity coefficient\n6 - Margules parameters using experimental data (T constant)\n0 - Exit"
 
 CSI = "\033["
 RESET = "\033[0m"
@@ -56,16 +59,66 @@ def center_text(text: str) -> str:
     lines = text.split("\n")
     return "\n".join(line.center(width) for line in lines)
 
-#=====================================================================#
+#===========================================================#
+
+def read_float(prompt):
+    value = input(prompt)
+    if value.strip() == "":    
+        return None
+    return float(value)
+
 
 def cli():
+   
     centered_logo = center_text(LOGO)
     centered_colorized_logo = colorize(centered_logo)
     print(centered_colorized_logo)
     print(center_text(VERSION))
     print("\n")
     print((MESSAGE))
-    print(COMMANDS)
+    
+    while True:
+        
+        print(COMMANDS)
+        print("\n")
+        
+        a = int(input("=> "))
+        print("\n")
+        if a == 1:
+            print("Leave empty the one you want to calculate:")
+            print("(V in cm³/mol)\n")
+            P=read_float("P: ")
+            V=read_float("v: ")
+            T=read_float("T: ")
+            PVTideal(P,V,T)
+        elif a == 2:
+            print("Leave empty the one you want to calculate:")
+            print("(V in cm³/mol)\n")
+            P=read_float("P: ")
+            V=read_float("v: ")
+            T=read_float("T: ")
+            Pc=read_float("Pc: ")
+            Tc=read_float("Tc: ")
+            Omega=read_float("Acentric factor: ")
+        elif a == 3:
+            print("Leave empty the one you want to calculate:")
+            print("(V in cm³/mol)\n")
+            P=read_float("P: ")
+            V=read_float("v: ")
+            T=read_float("T: ")
+            Pc=read_float("Pc: ")
+            Tc=read_float("Tc: ")
+            Omega=read_float("Acentric factor: ")
+        elif a == 4:
+            print("Fugacity selected.\n")
+        elif a == 5:
+            print("Activity coefficient selected.\n")
+        elif a == 6:
+            print("Margules parameters selected.\n")
+        elif a == 0:
+            break
+        else:
+            print("Invalid input.\n")
 
 if __name__ == "__main__":
     cli()
